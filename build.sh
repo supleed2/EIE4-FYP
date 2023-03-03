81b2d004a5835dfd0539f84f3c003ce8531367d1
Aadi Desai <21363892+supleed2@users.noreply.github.com>
2023-03-03 17:03:43 +0000

    Update build script
    Exit on error
    Build documentation & move to project root
    Move built bitstream to Win11 Desktop

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-python3 make.py --build
+set -e
+python3 make.py --build --doc
 cp build/gsd_orangecrab/gateware/gsd_orangecrab.bit gsd_orangecrab.dfu && echo "Copied .bit to .dfu"
 dfu-suffix -v 1209 -p 5af0 -a gsd_orangecrab.dfu
 rm -f /mnt/c/Users/suple/Desktop/dfu-util-0.9-win64/gsd.dfu && echo "Deleted old Win11 DFU"
 mv gsd_orangecrab.dfu /mnt/c/Users/suple/Desktop/dfu-util-0.9-win64/gsd.dfu && echo "Moved new DFU to Win11 Desktop"
+rm -f docs/ && echo "Deleted old docs in project root"
+cp -r build/gsd_orangecrab/doc/_build/html docs/ && echo "Copied docs to project root"
