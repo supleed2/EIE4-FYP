3439c7666e7b352846d89e77b7f4a4fbc6bb1b1c
Aadi Desai <21363892+supleed2@users.noreply.github.com>
2023-03-13 18:40:23 +0000

    Comment Win11 copy from build and demo scripts

--- demo.sh
+++ demo.sh
@@ -2,5 +2,5 @@
 set -e
 python3 demo/demo.py --build-path build/gsd_orangecrab/ --with-cxx
 rm -f demo/*.o demo/*.d demo/demo.bin demo/demo.elf demo/demo.elf.map && echo "Cleaned up build artifacts"
-rm -f /mnt/c/Users/suple/Desktop/dfu-util-0.9-win64/gsd.bin && echo "Deleted old Win11 BIN"
-mv demo.bin /mnt/c/Users/suple/Desktop/dfu-util-0.9-win64/gsd.bin && echo "Moved new BIN to Win11 Desktop"
+# rm -f /mnt/c/Users/suple/Desktop/dfu-util-0.9-win64/gsd.bin && echo "Deleted old Win11 BIN"
+# mv demo.bin /mnt/c/Users/suple/Desktop/dfu-util-0.9-win64/gsd.bin && echo "Moved new BIN to Win11 Desktop"
