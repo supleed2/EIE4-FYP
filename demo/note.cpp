4c831539a8b4818edaa99bc9cdf3a43faedd603b
Aadi Desai <21363892+supleed2@users.noreply.github.com>
2023-05-22 13:26:37 +0100

    Add ifdefs for CSR in note header and source

--- demo/note.cpp
+++ demo/note.cpp
@@ -1,6 +1,7 @@
 #include <generated/csr.h>
 #include <stdint.h>
 
+#ifdef CSR_AUDIO_BASE
 void note(uint32_t frequency, unsigned int duration_ms) {
 	audio_targ0_write(frequency);
 	busy_wait(duration_ms);
@@ -10,3 +11,4 @@ void note(uint32_t frequency, unsigned int duration_ms) {
 void wave(uint32_t wave) {
 	audio_wave0_write(wave);
 }
+#endif
