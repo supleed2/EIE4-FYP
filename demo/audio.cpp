f1dc882f934b0fc7970f253ad24bd6c94d9ac85c
Aadi Desai <21363892+supleed2@users.noreply.github.com>
2023-06-08 01:25:02 +0100

    Remove float from `audio` headers and helper files

--- demo/audio.cpp
+++ demo/audio.cpp
@@ -3,11 +3,6 @@
 #include <stdint.h>
 
 #ifdef CSR_AUDIO_BASE
-// Normalise target frequency from float to (24.4 bit) fixed point
-static inline uint32_t n_tf(float freq) {
-	return static_cast<uint32_t>(freq * 16.0f);
-}
-
 // Set all oscillators to (0Hz, sawtooth)
 void reset_audio(void) {
 	for (int i = 0; i < 64; i++) {
@@ -24,22 +19,22 @@ void set_wave(uint32_t osc, wave_t wave) {
 }
 
 // Set oscillator `osc` to frequency `freq`Hz
-void set_freq(uint32_t osc, float freq) {
+void set_freq(uint32_t osc, note_t freq) {
 	audio_osc_write(osc);
-	audio_tf_write(n_tf(freq));
+	audio_tf_write(freq);
 }
 
 // Set oscillator `osc` to waveform `wave` at frequency `freq`Hz
-void audio(uint32_t osc, wave_t wave, float freq) {
+void audio(uint32_t osc, wave_t wave, note_t freq) {
 	audio_osc_write(osc);
 	audio_wav_write(wave);
-	audio_tf_write(n_tf(freq));
+	audio_tf_write(freq);
 }
 
 // Set oscillator `osc` to waveform `wave` at frequency `freq`Hz for `ms` milliseconds
-void timed_freq(uint32_t osc, float freq, unsigned int ms) {
+void timed_freq(uint32_t osc, note_t freq, unsigned int ms) {
 	audio_osc_write(osc);
-	audio_tf_write(n_tf(freq));
+	audio_tf_write(freq);
 	busy_wait(ms);
 	audio_tf_write(0);
 }
